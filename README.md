8ea6e01ec8af6d6f1d91c76cee33441f11811d93
Igor C. de Paula <principe.borodin@gmail.com>
2015-09-10 15:43:15 -0300

    Add script and updated readme

--- README.md
+++ README.md
@@ -5,4 +5,23 @@ Script for install some tools for web development on ubuntu
 
 ## Instruction
 
-Download the script, set chmod a+x on script and run with root mode.
+Download the script, open the terminal and write
+```$ chmod a+x on ./script.sh 
+   $ sudo sh script.sh
+```
+
+
+## Tools on script
+
+
++[PHP5](http://www.php.net/)
++[Apache Web Server](http://httpd.apache.org/)
++[MySQL](https://www.mysql.com/)
++unzip (to phpMyAdmin)
++[phpMyAdmin](http://phpmyadmin.net)
++[Node.JS][http://www.nodejs.org] 
++[cURL](http://curl.haxx.se/)
++[Java (to netbeans)](www.java.com)
++[VirtualBox (to virtualization)](https://www.virtualbox.org/)
++[Vagrant (to shared virtualization)](https://www.vagrantup.com/)
++[Docker (to application containers)](https://www.docker.com/)
---
+++ script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "## Atualizando lista de pacotes"
+apt-get update
+echo "## Atualizando pacotes desatualizados desde o lancamento"
+apt-get upgrade -y
+echo "## Instalando PHP5"
+apt-get install -y php5
+echo "## Instalando Apache2 e modulo para php"
+apt-get install -y apache2 libapache2-mod-php5
+service apache2 restart
+echo "## Instalando MySQL, Digite a senha quando solicitado"
+apt-get install -y mysql-server mysql-client
+echo "## Instalando mysql para php"
+apt-get install -y php5-mysql
+echo "## Instalando unzip e phpMyAdmin"
+apt-get install -y unzip
+wget https://phpmyadmin-downloads-532693.c.cdn77.org/phpMyAdmin/4.4.9/phpMyAdmin-4.4.9-all-languages.zip
+unzip phpMyAdmin-4.4.9-all-languages.zip
+cp -R phpMyAdmin* /var/www/html
+echo "## Instalando nodejs"
+apt-get install -y curl
+curl -sL https://deb.nodesource.com/setup_0.12 | bash -
+apt-get install nodejs
+echo "## Instalando o java para o netbeans"
+apt-get install -y openjdk-8-jre openjdk-8-jdk
+echo "## Instalando virtualbox"
+wget http://download.virtualbox.org/virtualbox/4.3.28/virtualbox-4.3_4.3.28-100309~Ubuntu~raring_amd64.deb
+dpkg -i virtualbox-4.3_4.3.28-100309~Ubuntu~raring_amd64.deb
+echo "## Instalando o vagrant"
+wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb
+dpkg -i vagrant_1.7.2_x86_64.deb
+echo "## Instalando o docker"
+apt-get install -y docker.io
+echo "## Script finalizado. Visite: docker.io / vagrantup.com / vagrantbox.es / nodejs.org / php.net"
